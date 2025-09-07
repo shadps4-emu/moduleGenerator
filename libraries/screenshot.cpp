bfe3f3944e251c23c6f5ec7cf0c75a9ac5448db2
Stephen Miller <millerste004@gmail.com>
2025-09-06 21:59:49 -0500

    Regenerate libs
    Why not 😄

--- libraries/screenshot.cpp
+++ libraries/screenshot.cpp
@@ -8,11 +8,6 @@
 
 namespace Libraries::ScreenShot {
 
-s32 PS4_SYSV_ABI sceScreenShotSetDrcParam() {
-    LOG_ERROR(Lib_ScreenShot, "(STUBBED) called");
-    return ORBIS_OK;
-}
-
 s32 PS4_SYSV_ABI _Z5dummyv() {
     LOG_ERROR(Lib_ScreenShot, "(STUBBED) called");
     return ORBIS_OK;
@@ -78,34 +73,35 @@ s32 PS4_SYSV_ABI sceScreenShotSetParam() {
     return ORBIS_OK;
 }
 
+s32 PS4_SYSV_ABI sceScreenShotSetDrcParam() {
+    LOG_ERROR(Lib_ScreenShot, "(STUBBED) called");
+    return ORBIS_OK;
+}
+
 void RegisterLib(Core::Loader::SymbolsResolver* sym) {
-    LIB_FUNCTION("itlWFWV3Tzc", "libSceScreenShotDrc", 1, "libSceScreenShot", 0, 9,
-                 sceScreenShotSetDrcParam);
-    LIB_FUNCTION("AS45QoYHjc4", "libSceScreenShot", 1, "libSceScreenShot", 0, 9, _Z5dummyv);
-    LIB_FUNCTION("JuMLLmmvRgk", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
-                 sceScreenShotCapture);
-    LIB_FUNCTION("tIYf0W5VTi8", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
-                 sceScreenShotDisable);
-    LIB_FUNCTION("ysfza71rm9M", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("AS45QoYHjc4", "libSceScreenShot", 1, "libSceScreenShot", _Z5dummyv);
+    LIB_FUNCTION("JuMLLmmvRgk", "libSceScreenShot", 1, "libSceScreenShot", sceScreenShotCapture);
+    LIB_FUNCTION("tIYf0W5VTi8", "libSceScreenShot", 1, "libSceScreenShot", sceScreenShotDisable);
+    LIB_FUNCTION("ysfza71rm9M", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotDisableNotification);
-    LIB_FUNCTION("2xxUtuC-RzE", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
-                 sceScreenShotEnable);
-    LIB_FUNCTION("BDUaqlVdSAY", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("2xxUtuC-RzE", "libSceScreenShot", 1, "libSceScreenShot", sceScreenShotEnable);
+    LIB_FUNCTION("BDUaqlVdSAY", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotEnableNotification);
-    LIB_FUNCTION("hNmK4SdhPT0", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("hNmK4SdhPT0", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotGetAppInfo);
-    LIB_FUNCTION("VlAQIgXa2R0", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("VlAQIgXa2R0", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotGetDrcParam);
-    LIB_FUNCTION("-SV-oTNGFQk", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("-SV-oTNGFQk", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotIsDisabled);
-    LIB_FUNCTION("ICNJ-1POs84", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("ICNJ-1POs84", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotIsVshScreenCaptureDisabled);
-    LIB_FUNCTION("ahHhOf+QNkQ", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("ahHhOf+QNkQ", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotSetOverlayImage);
-    LIB_FUNCTION("73WQ4Jj0nJI", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
+    LIB_FUNCTION("73WQ4Jj0nJI", "libSceScreenShot", 1, "libSceScreenShot",
                  sceScreenShotSetOverlayImageWithOrigin);
-    LIB_FUNCTION("G7KlmIYFIZc", "libSceScreenShot", 1, "libSceScreenShot", 0, 9,
-                 sceScreenShotSetParam);
+    LIB_FUNCTION("G7KlmIYFIZc", "libSceScreenShot", 1, "libSceScreenShot", sceScreenShotSetParam);
+    LIB_FUNCTION("itlWFWV3Tzc", "libSceScreenShotDrc", 1, "libSceScreenShot",
+                 sceScreenShotSetDrcParam);
 };
 
 } // namespace Libraries::ScreenShot
